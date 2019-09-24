0d433ee51ff961b7e20ba08b85d709806baa157c
Greggy GIanini Firmansyah <greggygf@gmail.com>
2019-09-24 09:29:49 +0700

    blurry images

--- app/src/main/java/com/bluohazard/tourdengalam/MainMenuActivity.java
+++ app/src/main/java/com/bluohazard/tourdengalam/MainMenuActivity.java
@@ -2,7 +2,11 @@ package com.bluohazard.tourdengalam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.jackandphantom.blurimage.BlurImage;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        // Blur Images
+        ImageView imageView = findViewById(R.id.wallpaper);
+        Bitmap bitmap = BlurImage.with(getApplicationContext()).load(R.drawable.kampungbiru).intensity(20).Async(true).getImageBlur();
+        imageView.setImageBitmap(bitmap);
     }
 }
