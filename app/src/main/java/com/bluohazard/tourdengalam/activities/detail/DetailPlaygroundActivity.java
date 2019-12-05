fcc324739fe69cb61d4d7c6ca0a20ad0b0bae21a
Greggy GIanini Firmansyah <greggygf@gmail.com>
2019-12-05 14:07:35 +0700

    add detail mountain, detail playground, detail recommendation

---
+++ app/src/main/java/com/bluohazard/tourdengalam/activities/detail/DetailPlaygroundActivity.java
@@ -0,0 +1,14 @@
+package com.bluohazard.tourdengalam.activities.detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailPlaygroundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_playground);
+    }
+}
---
+++ app/src/main/java/com/bluohazard/tourdengalam/activities/detail/DetailRecommendationActivity.java
@@ -0,0 +1,16 @@
+package com.bluohazard.tourdengalam.activities.detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bluohazard.tourdengalam.R;
+
+public class DetailRecommendationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_recommendation);
+    }
+}
