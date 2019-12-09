e25f8b017d4c930f8838c48c7f426565a0105650
Greggy GIanini Firmansyah <greggygf@gmail.com>
2019-12-09 07:57:07 +0700

    clean

--- app/src/main/java/com/bluohazard/tourdengalam/activities/detail/DetailMountainActivity.java
+++ app/src/main/java/com/bluohazard/tourdengalam/activities/detail/DetailMountainActivity.java
@@ -1,13 +1,13 @@
 package com.bluohazard.tourdengalam.activities.detail;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bluohazard.tourdengalam.R;
 import com.bluohazard.tourdengalam.activities.list_vacation.MountainActivity;
