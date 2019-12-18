adbfc579551c2c411176e5b5416d7c5e1a44a777
Greggy GIanini Firmansyah <greggygf@gmail.com>
2019-12-18 14:54:45 +0700

    change the logo when dark mode

--- app/src/main/java/com/bluohazard/tourdengalam/activities/menu/AboutActivity.java
+++ app/src/main/java/com/bluohazard/tourdengalam/activities/menu/AboutActivity.java
@@ -3,6 +3,7 @@ package com.bluohazard.tourdengalam.activities.menu;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +14,8 @@ import com.bluohazard.tourdengalam.settings.SharedPref;
 public class AboutActivity extends AppCompatActivity {
 
     SharedPref sharedPref;
+
+    public ImageView imageLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,16 @@ public class AboutActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        imageLogo = findViewById(R.id.imageLogo);
+
+        // Change the logo when dark/day mode
+
+        if (sharedPref.loadNightModeState() == true) {
+            imageLogo.setImageResource(R.drawable.logo_dark);
+        } else {
+            imageLogo.setImageResource(R.drawable.logo);
+        }
     }
 
     public void onClickMainMenu(View view) {
--- app/src/main/java/com/bluohazard/tourdengalam/activities/menu/ListVacationActivity.java
+++ app/src/main/java/com/bluohazard/tourdengalam/activities/menu/ListVacationActivity.java
@@ -3,6 +3,7 @@ package com.bluohazard.tourdengalam.activities.menu;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +18,8 @@ public class ListVacationActivity extends AppCompatActivity {
 
     SharedPref sharedPref;
 
+    public ImageView beachLogo, mountainLogo, playgroundLogo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         sharedPref = new SharedPref(this);
@@ -26,9 +29,26 @@ public class ListVacationActivity extends AppCompatActivity {
         } else {
             setTheme(R.style.AppTheme);
         }
-        
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_vacation);
+
+        beachLogo = findViewById(R.id.beachLogo);
+        mountainLogo = findViewById(R.id.mountainLogo);
+        playgroundLogo = findViewById(R.id.playgroundLogo);
+
+        // Change the logo when dark/day mode
+
+        if (sharedPref.loadNightModeState() == true) {
+            beachLogo.setImageResource(R.drawable.sunset_dark);
+            mountainLogo.setImageResource(R.drawable.mountain_dark);
+            playgroundLogo.setImageResource(R.drawable.playground_dark);
+        } else {
+            beachLogo.setImageResource(R.drawable.sunset);
+            mountainLogo.setImageResource(R.drawable.mountain);
+            playgroundLogo.setImageResource(R.drawable.playground);
+        }
+
     }
 
     public void onClickMainMenu(View view) {
