e20da4217ba56d22b45284be4774986efdad42a9
Greggy GIanini Firmansyah <greggygf@gmail.com>
2019-12-18 19:00:22 +0700

    add fragments and animations

---
+++ app/src/main/java/com/bluohazard/tourdengalam/fragments/HistoryPhotoFragment.java
@@ -0,0 +1,32 @@
+package com.bluohazard.tourdengalam.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bluohazard.tourdengalam.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HistoryPhotoFragment extends Fragment {
+
+
+    public HistoryPhotoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history_photo, container, false);
+    }
+
+}
